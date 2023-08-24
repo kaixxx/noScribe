d7461de256d9dd100e9a41edee24eac4c470bdc8
kaixxx <whg.ffm@web.de>
2023-08-24 19:09:04 +0200

    New option to mark parallel speaking in transcript

--- noScribe.py
+++ noScribe.py
@@ -270,38 +270,52 @@ class App(ctk.CTk):
     
         # language
         self.label_language = ctk.CTkLabel(self.frame_options, text=t('label_language'))
-        self.label_language.grid(column=0, row=2, sticky='w', pady=10)
+        self.label_language.grid(column=0, row=2, sticky='w', pady=5)
 
         self.langs = ('auto', 'en (english)', 'zh (chinese)', 'de (german)', 'es (spanish)', 'ru (russian)', 'ko (korean)', 'fr (french)', 'ja (japanese)', 'pt (portuguese)', 'tr (turkish)', 'pl (polish)', 'ca (catalan)', 'nl (dutch)', 'ar (arabic)', 'sv (swedish)', 'it (italian)', 'id (indonesian)', 'hi (hindi)', 'fi (finnish)', 'vi (vietnamese)', 'iw (hebrew)', 'uk (ukrainian)', 'el (greek)', 'ms (malay)', 'cs (czech)', 'ro (romanian)', 'da (danish)', 'hu (hungarian)', 'ta (tamil)', 'no (norwegian)', 'th (thai)', 'ur (urdu)', 'hr (croatian)', 'bg (bulgarian)', 'lt (lithuanian)', 'la (latin)', 'mi (maori)', 'ml (malayalam)', 'cy (welsh)', 'sk (slovak)', 'te (telugu)', 'fa (persian)', 'lv (latvian)', 'bn (bengali)', 'sr (serbian)', 'az (azerbaijani)', 'sl (slovenian)', 'kn (kannada)', 'et (estonian)', 'mk (macedonian)', 'br (breton)', 'eu (basque)', 'is (icelandic)', 'hy (armenian)', 'ne (nepali)', 'mn (mongolian)', 'bs (bosnian)', 'kk (kazakh)', 'sq (albanian)', 'sw (swahili)', 'gl (galician)', 'mr (marathi)', 'pa (punjabi)', 'si (sinhala)', 'km (khmer)', 'sn (shona)', 'yo (yoruba)', 'so (somali)', 'af (afrikaans)', 'oc (occitan)', 'ka (georgian)', 'be (belarusian)', 'tg (tajik)', 'sd (sindhi)', 'gu (gujarati)', 'am (amharic)', 'yi (yiddish)', 'lo (lao)', 'uz (uzbek)', 'fo (faroese)', 'ht (haitian   creole)', 'ps (pashto)', 'tk (turkmen)', 'nn (nynorsk)', 'mt (maltese)', 'sa (sanskrit)', 'lb (luxembourgish)', 'my (myanmar)', 'bo (tibetan)', 'tl (tagalog)', 'mg (malagasy)', 'as (assamese)', 'tt (tatar)', 'haw (hawaiian)', 'ln (lingala)', 'ha (hausa)', 'ba (bashkir)', 'jw (javanese)', 'su (sundanese)')
 
         self.option_menu_language = ctk.CTkOptionMenu(self.frame_options, width=100, values=self.langs)
-        self.option_menu_language.grid(column=1, row=2, sticky='e', pady=10)
+        self.option_menu_language.grid(column=1, row=2, sticky='e', pady=5)
         try:
             self.option_menu_language.set(config['last_language'])
         except:
             pass
 
+        # Quality (Model Selection)
+        self.label_quality = ctk.CTkLabel(self.frame_options, text=t('label_quality'))
+        self.label_quality.grid(column=0, row=3, sticky='w', pady=5)
+        
+        self.option_menu_quality = ctk.CTkOptionMenu(self.frame_options, width=100, values=['precise', 'fast'])
+        self.option_menu_quality.grid(column=1, row=3, sticky='e', pady=5)
+        try:
+            self.option_menu_quality.set(config['last_quality'])
+        except:
+            pass
+
         # Speaker Detection (Diarization)
         self.label_speaker = ctk.CTkLabel(self.frame_options, text=t('label_speaker'))
-        self.label_speaker.grid(column=0, row=3, sticky='w', pady=10)
+        self.label_speaker.grid(column=0, row=4, sticky='w', pady=5)
 
         self.option_menu_speaker = ctk.CTkOptionMenu(self.frame_options, width=100, values=['auto', 'none'])
-        self.option_menu_speaker.grid(column=1, row=3, sticky='e', pady=10)
+        self.option_menu_speaker.grid(column=1, row=4, sticky='e', pady=5)
         try:
             self.option_menu_speaker.set(config['last_speaker'])
         except:
             pass
 
-        # Quality (Model Selection)
-        self.label_quality = ctk.CTkLabel(self.frame_options, text=t('label_quality'))
-        self.label_quality.grid(column=0, row=4, sticky='w', pady=10)
-        
-        self.option_menu_quality = ctk.CTkOptionMenu(self.frame_options, width=100, values=['precise', 'fast'])
-        self.option_menu_quality.grid(column=1, row=4, sticky='e', pady=10)
+        # Parallel Speaking (Diarization)
+        self.label_parallel = ctk.CTkLabel(self.frame_options, text=t('label_parallel'))
+        self.label_parallel.grid(column=0, row=5, sticky='w', pady=5)
+
+        self.check_box_parallel = ctk.CTkCheckBox(self.frame_options, text = '')
+        self.check_box_parallel.grid(column=1, row=5, sticky='e', pady=5)
         try:
-            self.option_menu_quality.set(config['last_quality'])
+            if config['last_parallel']:
+                self.check_box_parallel.select()
+            else:
+                self.check_box_parallel.deselect()
         except:
-            pass
+            self.check_box_parallel.select() # default to on
 
         # Start Button
         self.start_button = ctk.CTkButton(self.sidebar_frame, height=42, text=t('start_button'), command=self.button_start_event)
@@ -483,6 +497,8 @@ class App(ctk.CTk):
                     self.prompt = ''
             
             self.speaker_detection = self.option_menu_speaker.get()
+            
+            self.parallel = self.check_box_parallel.get()
 
             try:
                 if config['auto_save'] == 'True': # auto save during transcription (every 20 sec)?
@@ -616,7 +632,7 @@ class App(ctk.CTk):
                                 segment_len = segment.duration * 1000
                                 spkr = f'S{label[8:]}' # shorten the label: "SPEAKER_01" > "S01"
 
-                    if is_parallel:
+                    if self.parallel and is_parallel:
                         return f"//{spkr}"
                     else:
                         return spkr
@@ -925,6 +941,7 @@ class App(ctk.CTk):
             config['last_language'] = self.option_menu_language.get()
             config['last_speaker'] = self.option_menu_speaker.get()
             config['last_quality'] = self.option_menu_quality.get()
+            config['last_parallel'] = self.check_box_parallel.get()
             save_config()
         finally:
             self.destroy()
