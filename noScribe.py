785ae41185cef496408a5311e429a72500b71dc7
gernophil <gernophil@gmx.net>
2023-11-02 10:06:29 +0100

    moved import torch.backends.mps for safety reasons

--- noScribe.py
+++ noScribe.py
@@ -39,12 +39,12 @@ if platform.system() == 'Windows':
     from subprocess import STARTUPINFO, STARTF_USESHOWWINDOW
 import re
 if platform.system() == "Darwin": # = MAC
-    import torch.backends.mps
     if platform.machine() == "x86_64":
         os.environ['KMP_DUPLICATE_LIB_OK']='True' # prevent OMP: Error #15: Initializing libomp.dylib, but found libiomp5.dylib already initialized.
     # if platform.machine() == "arm64": # Intel should also support MPS
     if platform.mac_ver()[0] >= '12.3': # MPS needs macOS 12.3+
         os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = str(1)
+    import torch.backends.mps
 import AdvancedHTMLParser
 from threading import Thread
 import time
