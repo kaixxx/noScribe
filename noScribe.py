05ef51e182c68eda1d82557b1e1f37a9fb7d3a37
Y4hL <43219534+Y4hL@users.noreply.github.com>
2023-12-12 22:59:12 +0200

    Bug fix in refactored millisec function
    Forgot to add a * 1000, whoops

--- noScribe.py
+++ noScribe.py
@@ -153,7 +153,7 @@ def millisec(timeStr: str) -> int:
     """ Convert 'hh:mm:ss' string into milliseconds """
     try:
         h, m, s = timeStr.split(':')
-        return int(h) * 3600 + int(m) * 60 + int(s) # https://stackoverflow.com/a/6402859
+        return (int(h) * 3600 + int(m) * 60 + int(s)) * 1000 # https://stackoverflow.com/a/6402859
     except:
         raise Exception(t('err_invalid_time_string', time = timeStr))
 
