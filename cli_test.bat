609a48882b4e29d4021537f5d265cc41f8755e4a
kaixxx <whg.ffm@web.de>
2025-08-19 17:26:40 +0200

    Create cli_test.bat

---
+++ cli_test.bat
@@ -0,0 +1,3 @@
+﻿@echo off & chcp 65001 >nul
+
+python noScribe.py "C:\Users\kai\Documents\Programmierung\2023_WhisperTranscribe\Intw\Ein Gespräch mit Heikedine Körting kurz.mp3" "C:\Users\kai\Documents\Programmierung\2023_WhisperTranscribe\Intw\t.html"
